112e35eb6bf5f75830e406a2b7b30afa923ba010
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2021-02-20 09:57:28 -0800

    Remove unused code

--- src/Error.js
+++ src/Error.js
@@ -1,15 +1,6 @@
-import { useState } from "react";
-
-function Error(props) {
-
-  function resetErrors() {
-    // let errorBox = document.getElementById("error");
-    // errorBox.style.display = "none";
-    // errorBox.innerHTML = "";
-  }
-
-  if (props.error) {
-    return <div className="alert alert-danger mb-3">{props.error}</div>;
+function Error({error}) {
+  if (error) {
+    return <div className="alert alert-danger mb-3">{error}</div>;
   } else {
     return null;
   }
