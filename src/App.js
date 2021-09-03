676b2e14298dad2c22fa4d40b22474adbc72a798
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2021-09-02 21:14:24 -0700

    Format App css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
 import "./App.css";
+import React, { useState } from "react";
 import Connect from "./Connect";
 import Chat from "./Chat/Chat";
 import Rules from "./Rules";
