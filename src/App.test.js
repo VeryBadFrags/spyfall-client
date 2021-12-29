10eaf0c6f2d90f9b3d5f7aacd8130c24b206ce23
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2021-12-29 10:43:07 -0800

    Add fix for Jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
+/* eslint-env jest */
 test('renders learn react link', () => {
   render(<App />)
   const linkElement = screen.getByText(/Create lobby/i)
