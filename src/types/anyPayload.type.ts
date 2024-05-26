bca5a060056c100beb1ade4d3bd22f0b747bc9bb
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2024-05-26 11:47:07 -0700

    Run formatter

--- src/types/anyPayload.type.ts
+++ src/types/anyPayload.type.ts
@@ -2,4 +2,4 @@ import type { ChatPayload } from "./chatPayload.type";
 import type { LobbyStatusPayload } from "./lobbyStatus.type";
 import type { GamePayload } from "./socketPayload.type";
 
-export type AnyPayload = ChatPayload | GamePayload | LobbyStatusPayload
+export type AnyPayload = ChatPayload | GamePayload | LobbyStatusPayload;
