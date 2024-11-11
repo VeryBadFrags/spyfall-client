7e47774f31bf485b91478dd0ab4aa47e39961078
Loic Vourch <1664052+VeryBadFrags@users.noreply.github.com>
2024-11-10 17:14:48 -0800

    Focus on the chat box when the client was the last to send a message (#14)

--- src/types/chatPayload.type.ts
+++ src/types/chatPayload.type.ts
@@ -1,8 +1,10 @@
+import { ClientData } from "./clientData.type.ts";
+
 export type ChatPayload = {
   /** The message to send */
   message: string;
   /** The color of the message */
   color?: string;
   /** The author of the message */
-  author?: string;
+  author?: ClientData;
 };
