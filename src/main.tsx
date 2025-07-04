3906c86f34ea5febb704b2f78c7d4c2cb4c023e0
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2025-07-03 19:20:19 -0700

    Rename to App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app.tsx";
+import App from "./App.tsx";
 
 import "./styles/index.scss";
 import "@fontsource/atkinson-hyperlegible";
