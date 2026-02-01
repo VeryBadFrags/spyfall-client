4f7c37e24dcc21d277ecb5b60de09b76c8df749b
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2026-02-01 13:11:37 -0800

    feat: Basic storybook setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,22 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig, globalIgnores } from "eslint/config";
 
-export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
-    rules: {
-      "@typescript-eslint/consistent-type-imports": "error",
-      "sort-imports": [
-        "warn",
-        { ignoreCase: true, ignoreDeclarationSort: true },
-      ],
-    },
+export default defineConfig([{
+  files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+  plugins: { js },
+  extends: ["js/recommended"],
+  languageOptions: { globals: globals.browser },
+  rules: {
+    "@typescript-eslint/consistent-type-imports": "error",
+    "sort-imports": [
+      "warn",
+      { ignoreCase: true, ignoreDeclarationSort: true },
+    ],
   },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat["jsx-runtime"],
-  globalIgnores(["dist/"]),
-]);
+}, tseslint.configs.recommended, pluginReact.configs.flat["jsx-runtime"], globalIgnores(["dist/"]), ...storybook.configs["flat/recommended"]]);
