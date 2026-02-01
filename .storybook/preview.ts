a036312521bdd3bb6a71de4a1aa0f19b0982a710
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2026-02-01 13:20:03 -0800

    feat: add toast story

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,10 @@
 import type { Preview } from '@storybook/react-vite'
 
+import '../src/styles/index.scss';
+import '@fontsource/atkinson-hyperlegible';
+import '@fontsource/atkinson-hyperlegible/700.css';
+import '@fontsource/space-grotesk/600.css';
+
 const preview: Preview = {
   parameters: {
     controls: {
