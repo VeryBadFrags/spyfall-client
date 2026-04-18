84c89121307f53a21c268c15dba3fd02555d128b
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2026-04-18 12:30:46 -0700

    Replace Plausible with Umami

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Header from "@components/layout/Header/Header";
+import Footer from "@components/layout/Footer/Footer";
 
 import "./styles/index.scss";
 import "@fontsource/atkinson-hyperlegible";
 import "@fontsource/atkinson-hyperlegible/700.css";
 import "@fontsource/space-grotesk/600.css";
-
-import Plausible from "plausible-tracker";
-import Header from "@components/layout/Header/Header";
-import Footer from "@components/layout/Footer/Footer";
-const plausible = Plausible({
-  domain: "spyfall.verybadfrags.com",
-  apiHost: "/ps",
-  hashMode: false,
-});
-plausible.trackPageview();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
